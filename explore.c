998aff51901d4f90301e8d0277b6d22a1e77e103
Ehimare Okoyomon <eokoyomon@berkeley.edu>
2018-12-07 17:58:17 -0800

    got server in c and client in python running - successsssss

--- explore.c
+++ explore.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
 #include <netinet/in.h>
 #include <sys/socket.h>
@@ -80,37 +81,42 @@ bool start_instruction_server(int* server_fd, int* client_fd) {
 	int addrlen = sizeof(address);
 
 	// Creating socket file descriptor 
-	if ((*server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-		printf("Error initializing server socket\n");
+	if ((*server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		printf("Error initializing server socket\t%s\n", strerror(errno));
+
 		return false;
 	}
 
 	// Forcefully attaching socket to the port 8080 Part 1
-	if (setsockopt(*server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-		printf("Error initializing desired port - part 1\n");
+	if (setsockopt(*server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+		printf("Error initializing desired port - part 1:\t%s\n", strerror(errno));
 		return false;
 	}
 
+	memset(&address, 0, addrlen);
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(PORT);
 
 	// Forcefully attaching socket to the port 8080 Part 2
 	if (bind(*server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
-		printf("Error initializing desired port - part 2\n");
+		printf("Error initializing desired port - part 2\t%s\n", strerror(errno));
+
 		return false;
 	}
 
 	// Listen for 3 connections, just because..
 	if (listen(*server_fd, 3) < 0) {
-		printf("Error initializing server listening\n");
+		printf("Error initializing server listening\t%s\n", strerror(errno));
+
 		return false;
 	}
 
 	printf("Waiting for client computer to connect\n");
 
 	if ((*client_fd = accept(*server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-		printf("Error initializing connection to client for instruction\n");
+		printf("Error initializing connection to client for instruction\t%s\n", strerror(errno));
+;
 		return false;
 	}	
 
@@ -118,13 +124,15 @@ bool start_instruction_server(int* server_fd, int* client_fd) {
 	return true;
 }
 
+
 // Returns false if there was an error other than there just being no info
 // Info on select in http://beej.us/guide/bgnet/html/single/bgnet.html#blocking
 // Returns in the values for the variables passed in
 bool read_new_instruction(int client_fd, int* duck_detect_left, int* duck_detect_center, int* duck_detect_right, float* duck_dist) {
-	const int expected_bytes = 3*sizeof(int) + sizeof(float);
+	const int expected_bytes = 4*sizeof(int);
+	*duck_detect_left = 0, *duck_detect_center = 0, *duck_detect_right = 0, *duck_dist = 0;
 	char buffer[expected_bytes];
-	memset(buffer, 0, expected_bytes*sizeof(char));
+	memset(buffer, 0, expected_bytes);
 
 	struct timeval tv;
 	fd_set readfds;
@@ -147,12 +155,14 @@ bool read_new_instruction(int client_fd, int* duck_detect_left, int* duck_detect
 			return false;
 		}
 		if (nbytes == expected_bytes) {
+			printf("Got expected\n");
 			*duck_detect_left = *((int *) buffer);
 			*duck_detect_center = *(((int *) buffer) + 1);
 			*duck_detect_right = *(((int *) buffer) + 2);
 			*duck_dist = *( (float*) (((int *) buffer) + 3) );
 			return true;
 		}
+		printf("Did not get expected\n");
 	}
 
 	return true;
